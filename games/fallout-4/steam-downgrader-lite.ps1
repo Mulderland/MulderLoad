279b173d7e085f3c319a4be256814e835a1a7be6
mulderf0x <200639147+mulderf0x@users.noreply.github.com>
2026-08-16 13:05:26 +0200

    Revert "Temporary change domain name"
    This reverts commit 1d17865e3afd45106f1ec3ac5444427721e3820d.

--- games/fallout-4/steam-downgrader-lite.ps1
+++ games/fallout-4/steam-downgrader-lite.ps1
@@ -1,7 +1,7 @@
 # Disable progress bar
 $ProgressPreference = 'SilentlyContinue'
 
-Invoke-WebRequest -Uri "https://cdn.de.mulderland.com/games/fallout-4/steam-downgrader/1.11.221_lite/steam_depots_deltas.zip" -OutFile "steam_depots_deltas.zip"
+Invoke-WebRequest -Uri "https://cdn.mulderload.eu/games/fallout-4/steam-downgrader/1.11.221_lite/steam_depots_deltas.zip" -OutFile "steam_depots_deltas.zip"
 $hash = (Get-FileHash -Path ".\steam_depots_deltas.zip" -Algorithm SHA1 | Select-Object -ExpandProperty Hash).ToLower()
 if ($hash -ne "a0b24551b78ae273a0cad0eea351b10735724d41") {
     Write-Host "Error: downloaded file hash does not match expected value. Aborting."
